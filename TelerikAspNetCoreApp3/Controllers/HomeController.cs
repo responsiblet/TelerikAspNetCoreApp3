9650baf76b9fe229aaf50c379cf30c2602a3c615
Turner <andrew.turner@dxc.com>
2020-08-10 13:53:47 -0500

    Add project files.

---
+++ TelerikAspNetCoreApp3/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using Controller = Microsoft.AspNetCore.Mvc.Controller;
+using TelerikAspNetCoreApp3.Models;
+using JsonResult = Microsoft.AspNetCore.Mvc.JsonResult;
+
+namespace TelerikAspNetCoreApp3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["products"] = GetProducts();
+            return View(new ProductViewModel
+            {
+                ProductID = 4,
+                ProductName = "ProductName4"
+            });
+        }
+        public JsonResult GetProductsAjax()
+        {
+            var products = Enumerable.Range(0, 500).Select(i => new ProductViewModel
+            {
+                ProductID = i,
+                ProductName = "ProductName" + i
+            });
+
+            return Json(products, JsonRequestBehavior.AllowGet);
+        }
+        private static IEnumerable<ProductViewModel> GetProducts()
+        {
+            var products = Enumerable.Range(0, 2000).Select(i => new ProductViewModel
+            {
+                ProductID = i,
+                ProductName = "ProductName" + i
+            });
+
+            return products;
+        }
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ TelerikAspNetCoreApp3/Models/ProductViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TelerikAspNetCoreApp3.Models
+{
+    public class ProductViewModel
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+    }
+}
